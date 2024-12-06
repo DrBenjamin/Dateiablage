d3b06174cd3d37d13134f669075246491c348fe9
DrBenjamin <drdrbenjamin@icloud.com>
2024-12-06 15:58:28 +0100

    added 3rd control

--- Dateiablage.py
+++ Dateiablage.py
@@ -1,5 +1,6 @@
 import wx
 import os
+import io
 import subprocess
 import platform
 import unicodedata
@@ -14,8 +15,10 @@ class MyFrame(wx.Frame):
 
         # Creating the File menu
         file_menu = wx.Menu()
-        import_item = file_menu.Append(wx.ID_OPEN, "&Importiere e-Learning Definition")
+        import_definition = file_menu.Append(wx.ID_OPEN, "&Importiere e-Learning Definition")
+        import_tasks = file_menu.Append(wx.ID_ANY, "&Wähle Aufgabenliste")
         browse_item = file_menu.Append(wx.ID_ANY, "&Wähle Quellverzeichnis")
+        
         file_menu.Append(wx.ID_EXIT, "&Beenden")
         menu_bar.Append(file_menu, "&Datei")
         
@@ -29,26 +32,45 @@ class MyFrame(wx.Frame):
         # Setting the menu bar
         self.SetMenuBar(menu_bar)
 
+        # Creating a panel
         panel = wx.Panel(self)
-        sizer = wx.BoxSizer(wx.VERTICAL)
 
-        self.list_ctrl = wx.ListCtrl(panel,
+        # Creating the list controls
+        self.learning_ctrl = wx.ListCtrl(panel,
                                      style=wx.LC_REPORT
+                                     |wx.BORDER_SUNKEN
+                                     )
+        self.tasks_ctrl = wx.ListCtrl(panel,
+                                     style=wx.LC_ICON
                                      |wx.BORDER_SUNKEN
                                      )
         self.file_listbox = wx.ListBox(panel)
 
-        sizer.Add(self.list_ctrl, 1, wx.ALL | wx.EXPAND, 5)
-        sizer.Add(self.file_listbox, 1, wx.ALL | wx.EXPAND, 5)
-        panel.SetSizer(sizer)
+        # Creating a horizontal box sizer
+        hbox = wx.BoxSizer(wx.HORIZONTAL)
+        # Adding the list controls to the horizontal box sizer
+        hbox.Add(self.learning_ctrl, 1, wx.ALL | wx.EXPAND, 5)
+        hbox.Add(self.tasks_ctrl, 1, wx.ALL | wx.EXPAND, 5)
+
+        # Creating a vertical box sizer
+        vbox = wx.BoxSizer(wx.VERTICAL)
+        # Adding the horizontal box sizer and the file listbox to the vertical box sizer
+        vbox.Add(hbox, 1, wx.ALL | wx.EXPAND, 5)
+        vbox.Add(self.file_listbox, 1, wx.ALL | wx.EXPAND, 5)
+        # Setting the sizer for the frame and fit the panel
+        panel.SetSizer(vbox)
 
         # Binding the Import menu item to the on_import method
-        self.Bind(wx.EVT_MENU, self.on_import, import_item)
+        self.Bind(wx.EVT_MENU, self.on_import_csv, import_definition)
+        # Binding the Import menu item to the on_import method
+        self.Bind(wx.EVT_MENU, self.on_import_excel, import_tasks)
         # Binding the Browse menu item to the on_browse method
         self.Bind(wx.EVT_MENU, self.on_browse, browse_item)
 
         # Binding the list control to the on_item_activated method
-        self.list_ctrl.Bind(wx.EVT_LIST_ITEM_SELECTED, self.on_item_selected)
+        self.learning_ctrl.Bind(wx.EVT_LIST_ITEM_SELECTED, self.on_item_selected)
+        # Binding the list control to the on_item_activated method
+        self.tasks_ctrl.Bind(wx.EVT_LIST_ITEM_SELECTED, self.on_item_selected)
         # Binding the list control to the on_file_activated method
         self.file_listbox.Bind(wx.EVT_LISTBOX_DCLICK, self.on_file_activated)
 
@@ -60,41 +82,90 @@ class MyFrame(wx.Frame):
             self.list_files(self.folder_path)
         dialog.Destroy()
 
-    # Method to handle the Import menu item
-    def on_import(self, event):
+    # Method to handle the Import learning definition
+    def on_import_csv(self, event):
         dialog = wx.FileDialog(self, "Importiere e-Learning Definition", wildcard="CSV files (*.csv)|*.csv|All files (*.*)|*.*", style=wx.FD_OPEN | wx.FD_FILE_MUST_EXIST)
         if dialog.ShowModal() == wx.ID_OK:
             file_path = dialog.GetPath()
-            self.import_file(file_path)
+            self.import_csv(file_path)
         dialog.Destroy()
 
     # Method to import the CSV file
-    def import_file(self, file_path):
+    def import_csv(self, file_path):
         try:
-            wx.MessageBox(f"Datei erfolgreich importiert: {file_path}", "Success", wx.OK | wx.ICON_INFORMATION)
             definition_csv = pd.read_csv(file_path)
-            self.display_data(definition_csv)
+            self.display_learning(definition_csv)
+            wx.MessageBox(f"Datei erfolgreich importiert: {file_path}", "Erfolg", wx.OK | wx.ICON_INFORMATION)
         except Exception as e:
             wx.MessageBox(f"Datei nicht importiert: {e}", "Error", wx.OK | wx.ICON_ERROR)
 
-    # Method to display the data in the list control
-    def display_data(self, df):
-        self.list_ctrl.ClearAll()
-        self.list_ctrl.AppendColumn("e-Learning Struktur")
+    # Method to display the data in the learning control
+    def display_learning(self, df):
+        self.learning_ctrl.ClearAll()
+        self.learning_ctrl.AppendColumn("e-Learning Struktur")
 
         for index, row in df.iterrows():
             text = row.iloc[0]
             level = row.iloc[1]
             indent = ' ' * ((level * 4) - 4)  # Indent based on the level
-            self.list_ctrl.Append([f"{indent}{text}"])
+            self.learning_ctrl.Append([f"{indent}{text}"])
 
         # Adjusting the column width to fit automatically the content
-        self.list_ctrl.SetColumnWidth(0, wx.LIST_AUTOSIZE)
+        self.learning_ctrl.SetColumnWidth(0, wx.LIST_AUTOSIZE)
+
+    # Method to handle the Import tasks excel
+    def on_import_excel(self, event):
+        dialog = wx.FileDialog(self, "Importiere Aufgabenliste", wildcard="Exceldatei (*.xlsx)|*.xlsx|All files (*.*)|*.*", style=wx.FD_OPEN | wx.FD_FILE_MUST_EXIST)
+        if dialog.ShowModal() == wx.ID_OK:
+            file_path = dialog.GetPath()
+            self.import_excel(file_path)
+        dialog.Destroy()
+
+    # Method to import the Excel file
+    def import_excel(self, file_path):
+        try:
+            data = None
+            output = [[]]
+
+            # Reading file as bytes
+            with open(file_path, 'rb') as file:
+                bytes_data = file.read()
+
+            data = pd.read_excel(io.BytesIO(bytes_data), sheet_name = 1)
+
+            # Adding from Excel
+            for index, row in data.iterrows():
+                output.append([index, str(row.iloc[1]).strip().title()])
+
+            # Tasks
+            output = pd.DataFrame(output, columns = ['ID', 'Aufgabenliste'])
+            output = output.set_index('ID')
+            output = output.drop_duplicates(ignore_index = True)
+            output = output.drop(0, axis = 0)
+
+            self.display_tasks(output)
+            wx.MessageBox(f"Datei erfolgreich importiert: {file_path}", "Erfolg", wx.OK | wx.ICON_INFORMATION)
+        except Exception as e:
+            wx.MessageBox(f"Datei nicht importiert: {e}", "Error", wx.OK | wx.ICON_ERROR)
+
+    # Method to display the data in the learning control
+    def display_tasks(self, df):
+        self.learning_ctrl.ClearAll()
+        self.learning_ctrl.AppendColumn("e-Learning Struktur")
+
+        for index, row in df.iterrows():
+            text = row.iloc[0]
+            level = row.iloc[1]
+            indent = ' ' * ((level * 4) - 4)  # Indent based on the level
+            self.learning_ctrl.Append([f"{indent}{text}"])
+
+        # Adjusting the column width to fit automatically the content
+        self.learning_ctrl.SetColumnWidth(0, wx.LIST_AUTOSIZE)
 
     # Method to handle the list control item activated event
     def on_item_selected(self, event):
         item_index = event.GetIndex()
-        item_text = self.list_ctrl.GetItemText(item_index)
+        item_text = self.learning_ctrl.GetItemText(item_index)
         self.SetTitle(f"Dateiablage - {item_text.strip()}")
         self.list_files(self.folder_path, item_text.strip())
         
